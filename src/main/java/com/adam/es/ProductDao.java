36c8f995e580ad51e3ee679e07b26cf4851fa02e
Adam Li <da615@126.com>
2021-12-11 16:36:03 +0800

    es demo

---
+++ src/main/java/com/adam/es/ProductDao.java
@@ -0,0 +1,7 @@
+package com.adam.es;
+
+import org.springframework.data.elasticsearch.repository.ElasticsearchRepository;
+import org.springframework.stereotype.Repository;
+@Repository
+public interface ProductDao extends ElasticsearchRepository<Product,Long> {
+}
